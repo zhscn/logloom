b39db2714fd5252c2ee4c7798ed72337c935383d
zs <zs@ijk.dev>
2025-05-17 00:39:12 +0800

    log: handle empty __VA_ARGS__ in log macro

--- src/log.hh
+++ src/log.hh
@@ -7,23 +7,23 @@
 
 #define LOG_FNAME(name) static constexpr const char *FNAME = #name
 
-#define LOG_IMPLF(_log, _serverity, _spec, ...)                           \
-  /* NOLINTBEGIN */                                                       \
-  do {                                                                    \
-    char LOG_IMPL__buf[4096];                                             \
-    _log(_serverity) << [&] {                                             \
-      auto res =                                                          \
-          fmt::format_to(LOG_IMPL__buf, "{} " _spec, FNAME, __VA_ARGS__); \
-      if (res.truncated) [[unlikely]] {                                   \
-        LOG(WARNING) << "the next log is truncated";                      \
-      }                                                                   \
-      return std::string_view(LOG_IMPL__buf, res.out - LOG_IMPL__buf);    \
-    }();                                                                  \
+#define IMPL_LOGF_(_log, _serverity, _spec, ...)                       \
+  /* NOLINTBEGIN */                                                    \
+  do {                                                                 \
+    char LOG_IMPL__buf[4096];                                          \
+    _log(_serverity) << [&] {                                          \
+      auto res = fmt::format_to(LOG_IMPL__buf, "{} " _spec,            \
+                                FNAME __VA_OPT__(, ) __VA_ARGS__);     \
+      if (res.truncated) [[unlikely]] {                                \
+        LOG(WARNING) << "the next log is truncated";                   \
+      }                                                                \
+      return std::string_view(LOG_IMPL__buf, res.out - LOG_IMPL__buf); \
+    }();                                                               \
   } while (false); /* NOLINTEND */
 
-#define FATALF(...) LOG_IMPLF(LOG, FATAL, __VA_ARGS__)
-#define ERRORF(...) LOG_IMPLF(LOG, ERROR, __VA_ARGS__)
-#define WARNF(...) LOG_IMPLF(LOG, WARNING, __VA_ARGS__)
-#define INFOF(...) LOG_IMPLF(VLOG, 0, __VA_ARGS__)
-#define DEBUGF(...) LOG_IMPLF(VLOG, 1, __VA_ARGS__)
-#define TRACEF(...) LOG_IMPLF(VLOG, 2, __VA_ARGS__)
+#define FATALF(...) IMPL_LOGF_(LOG, FATAL, __VA_ARGS__)
+#define ERRORF(...) IMPL_LOGF_(LOG, ERROR, __VA_ARGS__)
+#define WARNF(...) IMPL_LOGF_(LOG, WARNING, __VA_ARGS__)
+#define INFOF(...) IMPL_LOGF_(VLOG, 0, __VA_ARGS__)
+#define DEBUGF(...) IMPL_LOGF_(VLOG, 1, __VA_ARGS__)
+#define TRACEF(...) IMPL_LOGF_(VLOG, 2, __VA_ARGS__)
